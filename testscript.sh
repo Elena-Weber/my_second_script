c0a09871fe9cca5eef114f68082bcf397fb89740
Elena-Weber <crochetmiracles@gmail.com>
2021-12-22 15:30:11 -0500

    basic search among files in current folder works

--- testscript.sh
+++ testscript.sh
@@ -4,6 +4,7 @@ echo
 echo "Hi! My name is BASH-SCRIPT and I was created by Elena Weber."
 echo
 
+# display all the folders and files in the current folder
 echo "Here is the list of folders and files in the current folder:"
 echo
 for anyfile in *;
@@ -16,9 +17,13 @@ echo "Let's take a closer look!"
 echo
 for anyfile in *;
 do
-    if [ -f $anyfile ]; then
-        echo "$anyfile is a file"
+# grep = find
+# -n = show the line on which the word is
+# -i = not pay attention to capital and small letters in search
+    if [ -f "$anyfile" ]; then
+        echo "+ $anyfile is a file"
+        grep -ni "once" "$anyfile"
     else
-        echo "$anyfile is a folder"
+        echo "+ $anyfile is a folder"
     fi
 done
