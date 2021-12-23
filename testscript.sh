71d5b63d549f4d010410705264ff7f134fff9fb8
Elena-Weber <crochetmiracles@gmail.com>
2021-12-23 17:50:17 -0500

    display works fine

--- testscript.sh
+++ testscript.sh
@@ -4,28 +4,23 @@ echo
 echo "Hi! My name is SCRIPT-THE-SECOND and I was created by Elena Weber."
 echo
 
-# display all the folders and files
-# echo "Here is the list of all folders and files in the current folder:"
-# echo
-# for anything in * ;
-# for anything in $(find . ! -path '*/\.*') ;
-# do
-#     namefile=$(basename "$anything")
-#     echo " * "$anything""
-#     echo "$namefile"
-# done
+# display all the folders and files (excl. *sh and hidden ones)
+echo "Here is the list of all folders and files in the current folder:"
+echo
+find . -type f ! -name '*.sh' ! -path '*/\.*' -exec echo "* {}" \;
+echo
 
 # set up the searched word
 read -p "What word are you looking for? " searchword
 echo
-if [ -z $searchword ]; then # if user doen't type anything
+if [ -z $searchword ]; then # if user doesn't type anything
     echo "Please restart the script and type the word you're looking for."
     echo
 else
     echo "Searching for -> $searchword <- in all folders and files (excl. hidden ones):"
     echo
     # exclude any hidden files and folders from search
-    for folder in $(find . -type d ! -path '*/\.*') ;
+    for folder in $(find . -type d ! -path '*/\.*' ! -name '*.sh') ;
     do
         echo "-> -> -> Searching in folder: $folder <- <- <-"
         echo
