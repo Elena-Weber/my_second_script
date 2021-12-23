24f0c94cd176e244d8ed3a978038c1bfaaf2db73
Elena-Weber <crochetmiracles@gmail.com>
2021-12-23 13:22:09 -0500

    remove unnecessary code

--- testscript.sh
+++ testscript.sh
@@ -1,78 +1,52 @@
 #! /bin/bash
 
 echo
-echo "Hi! My name is BASH-SHELL-SCRIPT and I was created by Elena Weber."
+echo "Hi! My name is BASH-SHELL-SCRIPT-THE-SECOND and I was created by Elena Weber."
 echo
 
-# display all the folders and files in the current folder
-echo "Here is the list of all folders and files in the current folder:"
-echo
-for anyfile in *;
-do
-    echo "* $anyfile"
-done
-
-echo
-echo "Let's take a closer look!"
-echo
-
-# for-loop going through all files in current folder
-# grep = find
-# -n = show the line on which the word is
-# -i = do not pay attention to capital/small letters in search
-for anyfile in *;
-do
-    if [ -f "$anyfile" ]; then
-        echo "+ $anyfile is a file"
-        grep -ni "once" "$anyfile"
-    else
-        echo "+ $anyfile is a folder"
-    fi
-done
-echo
-
-# for anyfile in *;
+# display all the folders and files
+# echo "Here is the list of all folders and files in the current folder:"
+# echo
+# for anything in * ;
+# for anything in $(find . ! -path '*/\.*') ;
 # do
-#     if [ -f "$anyfile" ]; then
-#         echo "+ $anyfile is a file"
-#         # grep -ni "once" "$anyfile"
-#         present=$(grep -ni "once" "$anyfile")
-#         if [ -z "$present" ]; then
-#             echo "The searched word was NOT found"
-#         else
-#             echo "The searched word was found"
-#         fi
-#     else
-#         echo "+ $anyfile is a folder"
-#     fi
+#     namefile=$(basename "$anything")
+#     echo " * "$anything""
+#     echo "$namefile"
 # done
 
+read -p "What word are you looking for? " searchrequest
+echo "$searchrequest"
+
 echo
-echo "All files and folders:"
+echo "Searching in all folders and files (excl. hidden ones):"
 echo
 
-# exclude any hidden files and folders
-for eachff in $(find . -type d ! -path '*/\.*') ;
+# exclude any hidden files and folders from search
+for folder in $(find . -type d ! -path '*/\.*') ;
 do
-    if [ $eachff != "./.git" ]; then
-        echo "*** Searching in folder: $eachff ***"
-        for myfile in $eachff/* ;
+    # if [ $folder != "./.git" ]; then
+        echo "-> -> -> Searching in folder: $folder <- <- <-"
+        echo
+        for anything in $folder/* ;
         do
-            if [ -f "$myfile" ]; then
-                echo "This file's path: $myfile"
-                present=$(grep -ni "once" "$myfile")
-                if [ -z "$present" ]; then
-                    filename=$(basename "$myfile")
-                    echo "The searched word was NOT found in "$filename""
+            if [ -f "$anything" ]; then
+                # grep = find
+                # -n = show the line on which the word is
+                # -i = ignore big/small letters in search
+                searched=$(grep -ni "once" "$anything")
+                if [ -z "$searched" ]; then
+                    filename=$(basename "$anything")
+                    echo "NOT found in -> "$filename" <-"
                     echo
                 else
-                    filename=$(basename "$myfile")
-                    echo "The searched word was FOUND in "$filename""
-                    # echo "$filename"
+                    filename=$(basename "$anything")
+                    echo "FOUND in -> "$filename" <- on line:"
+                    echo "$searched"
                     echo
                 fi
             fi
         done
-    fi
+    # fi
     echo
 done
