fe5405df3df430ea8c27ad0a184cf2eae5039801
Elena-Weber <crochetmiracles@gmail.com>
2022-01-09 16:28:32 -0500

    function refactor

--- testscript.sh
+++ testscript.sh
@@ -5,7 +5,8 @@ echo " - - - - - - - - - - - - - - - "
 echo
 echo "Hi! My name is SCRIPT-THE-SECOND and I was created by Elena Weber."
 echo "My job is to help you search for any word you want, and I love it!"
-echo "I look through all the files in the current folder."
+echo "I look through all the files and folders in the current folder."
+echo
 echo "Let's start! :)"
 echo
 
@@ -22,54 +23,64 @@ echo
 # set up the searched word
 read -p "What word are you looking for? " searchword
 echo
-if [ -z $searchword ]; then # if user doesn't type anything
-    echo "Please restart the script and type the word you're looking for."
-    echo
-else
-    echo "Searching for -> $searchword <- :"
-    echo
-    # exclude any hidden files and folders from search
-    for folder in $(find . -type d ! -path '*/\.*' ! -name '*.sh') ;
-    do
-        echo "-> -> -> Searching in folder: $folder <- <- <-"
-        echo
-        for anything in $folder/* ;
-        do
-            if [ -f "$anything" ]; then
-                # grep = find
-                # -n = show the line on which the word is
-                # -i = ignore big/small letters in search
-                searched=$(grep -ni "$searchword" "$anything")
-                if [ -z "$searched" ]; then
-                    filename=$(basename "$anything")
-                    echo "NOT found in -> "$filename" <-"
-                    echo
-                else
-                    filename=$(basename "$anything")
-                    echo "FOUND in -> "$filename" <- on line(s):"
-                    echo "$searched"
-                    echo
-                fi
-            fi
-        done
-        echo
-    done
-fi
 
-read -p "Would you like to search for another word? (y/N) " answer
+myfunction(){
+    if [ -z $searchword ]; then # if user doesn't type anything
+        echo "Please restart the script and type the word you're looking for."
+        echo
+    else
+        echo "Searching for -> $searchword <- :"
+        echo
+        # exclude any hidden files and folders from search
+        for folder in $(find . -type d ! -path '*/\.*' ! -name '*.sh') ;
+        do
+            echo "-> -> -> Searching in folder: $folder <- <- <-"
+            echo
+            for anything in $folder/* ;
+            do
+                if [ -f "$anything" ]; then
+                    # grep = find
+                    # -n = show the line on which the word is
+                    # -i = ignore big/small letters in search
+                    searched=$(grep -ni "$searchword" "$anything")
+                    if [ -z "$searched" ]; then
+                        filename=$(basename "$anything")
+                        echo "NOT found in -> "$filename" <-"
+                        echo
+                    else
+                        filename=$(basename "$anything")
+                        echo "FOUND in -> "$filename" <- on line(s):"
+                        echo "$searched"
+                        echo
+                    fi
+                fi
+            done
+            echo
+        done
+    fi
+
+    read -p "Would you like to search for another word? (y/N) " answer
+    echo
+    case $answer in
+        "" | "n" | "N")
+            echo "No problem. Thanks for using me! Have a great one!"
+            echo
+            echo " - - - - - - - - - - - - - - - "
+            echo
+            ;;
+        "y" | "Y")
+            read -p "What word are you looking for? " searchword
+            echo
+            myfunction
+            ;;
+        *)
+            echo "I'm sorry I don't understand. Thanks for using me! Have a great one!"
+            echo
+            echo " - - - - - - - - - - - - - - - "
+            echo
+            ;;
+    esac
+}
+
 echo
-case $answer in
-    "")
-        echo "No problem. Thanks for using me! Have a great one!"
-        ;;
-    "y" | "Y")
-        echo "What word are you looking for?"
-        ;;
-    "n" | "N")
-        echo "No problem. Thanks for using me! Have a great one!"
-        ;;
-    *)
-        echo "I'm sorry I don't understand. Thanks for using me! Have a great one!"
-        ;;
-esac
-echo
+myfunction
